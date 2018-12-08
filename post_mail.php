2ab90f9e1f9848c46b1aa8cdf84ac689588cb37e
kin29 <kin29.com@gmail.com>
2018-12-08 21:22:58 +0900

    first commit

---
+++ post_mail.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * 送信元・送信先・SendGridのAPIキーはConfig Varsにより登録(変更)可能
+ * なため、様々なメール送信ができるので汎用性あります。
+ */
+require 'vendor/autoload.php';    //SendGridのために。dotenvは元から入ってるっぽい・・・。
+
+$text = $_POST['text'];
+$userName = $_POST['user_name'];
+
+$arrText = explode('|', $text);
+$mailTitle = $arrText[0];
+$mailContent = $arrText[1];
+
+//不正チェック
+if ($_POST['token'] != getenv('APP_VERIFICATION_TOKEN')) {
+    postSlack('不正tokenのため、メール送信しませんでした。', $mailTitle, $mailContent);
+    return false;
+}
+
+//メール送信(SendGrid)
+$statusCode = sendMail($mailTitle, $mailContent);
+
+$sendResult = ($statusCode == 202) ? 'メール送信に成功しました。' : 'メール送信に失敗しました。';
+$sendResult .= "({$userName} さんにより実行されました。)";
+
+//slackにメール送信結果を投稿する
+postSlack($sendResult, $mailTitle, $mailContent);
+
+
+/**
+ * SendGridを使用してメール送信を行う。
+ * 送信元・送信先・SendGridのAPIキーはConfig Varsにより登録
+ *
+ * @param string $title メール件名
+ * @param string $content メール内容
+ * @return string $statusCode ステータスコード
+ */
+function sendMail($title, $content)
+{
+    $to = getenv('TO_ADDRESS');
+    $from = getenv('FROM_ADDRESS');
+
+    //herokuではメール送信できないため以下は使えない。
+    /*
+    $header = "From :{$from}\r\n";
+    mb_language('Japanese');
+    mb_internal_encoding('UTF-8');
+    return mb_send_mail($to, $title, $content, $header);
+    */
+
+    //SendGridを使用する
+    $from = new SendGrid\Email(null, $from);
+    $subject = $title;
+    $to = new SendGrid\Email(null, $to);
+    $content = new SendGrid\Content('text/plain', $content);
+    $mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+    $apiKey = getenv('SENDGRID_API_KEY');
+    $sg = new \SendGrid($apiKey);
+
+    $response = $sg->client->mail()->send()->post($mail);
+    return $response->statusCode();
+
+    //$response->headers();
+    //$response->body();
+}
+
+/**
+ * IncomingWebHookへPOSTし、slackへ投稿する。
+ * (投稿先のchannelは　slack API > app > Incoming Webhooks より設定する)
+ *
+ * @param string $sendResult メール送信結果
+ * @param string $title メール件名
+ * @param string $contents メール内容
+ */
+function postSlack($sendResult, $title, $contents)
+{
+    $payload = array(
+        'attachments' => array(
+            array(
+                'fallback' => $sendResult,
+                'pretext' => $sendResult,
+                'color' => '#A4C745',
+                'fields' => array(
+                    array(
+                        'title' => '【件名】',
+                        'value' => $title,
+                        'short' => false
+                    ),
+                    array(
+                        'title' => '【内容】',
+                        'value' => $contents,
+                        'short' => false
+                    ),
+                )
+            )
+        )
+    );
+
+    $IncomingWebHookURL = getenv('INCOMING_WEBHOOKURL');
+    $curl = curl_init($IncomingWebHookURL);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($payload));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($curl);
+    curl_close($curl);
+}
